ad155569bf2b6f213d9430257da5a43bc4cc0877
gaalbrag <gaalbrag@gmail.com>
2025-06-14 11:20:40 -0300

    removido LOGO do header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,14 +1,13 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { APP_NAME, LOGO_SVG } from '../../constants';
+import { APP_NAME} from '../../constants';
 
 const Header: React.FC = () => {
   return (
     <header className="bg-primary-dark text-text-light shadow-md">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         <Link to="/" className="flex items-center text-xl font-bold">
-          {LOGO_SVG}
           <span>{APP_NAME}</span>
         </Link>
         {/* User profile / settings can go here */}
